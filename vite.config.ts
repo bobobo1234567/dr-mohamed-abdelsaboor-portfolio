f2a50544a7a53784b1a319f5479e692bbc950c82
Ahmed Ezzat <bobobobobo722@gmail.com>
2025-05-24 14:36:16 +0300

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/dr-mohamed-abdelsaboor-portfolio/', // 👈 اسم الريبو على GitHub
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
